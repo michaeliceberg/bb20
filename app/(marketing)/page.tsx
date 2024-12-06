7a6903fbdee0b7d857574f2473c0fbe7e874b22d
Mikhail Korolev <shkolakorolev@gmail.com>
2024-12-06 15:29:00 +0300

    edit marketing 2 no lottie

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -19,9 +19,9 @@ export default function Home() {
 		<div className='max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2'>
 			<div className='relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0'>
 				
-				<Lottie className="h-120 w-120 pr-5"
+				{/* <Lottie className="h-120 w-120 pr-5"
 						animationData={ LottieHelloBread } 
-				/>
+				/> */}
 				
 				{/* <Image src='/hero.svg' fill alt='Hero' /> */}
 			</div>
